748a73ed518d66f798ead83857c24e3cda81bc85
simon123h <simon123h@users.noreply.github.com>
2020-07-17 12:44:04 +0200

    Move LVE and SHE demo problem implementations to their driver codes

--- lve.py
+++ lve.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3
-from src.demo_problems import LotkaVolterra
+from src.problem import Problem
 import numpy as np
 
 import matplotlib
 matplotlib.use("GTK3Agg")
 import matplotlib.pyplot as plt
 
-lve = LotkaVolterra()
+# The Lotka-Volterra equations (predator prey model)
+class LotkaVolterra(Problem):
 
-data = []
+    def __init__(self):
+        super().__init__()
+        self.a = 1
+        self.b = 1
+        self.c = 1
+        self.d = 1
+        self.u = np.array([1., 0.7])
+
+    def rhs(self, u):
+        x = u[0]
+        y = u[1]
+        return np.array([
+            (self.a - self.b * y) * x,
+            (self.d * x - self.c) * y
+        ])
+
+problem = LotkaVolterra()
+
+dat = []
 
 for n in range(10000):
-    data.append(lve.u.copy())
-    lve.time_step()
-    # print("Step #{:05d}".format(n))
+    dat.append(problem.u.copy())
+    problem.time_step()
 
-data = np.array(data).T
-plt.plot(data[0], data[1])
+dat = np.array(dat).T
+plt.plot(dat[0], dat[1])
 plt.show()
 
--- she.py
+++ she.py
@@ -1,34 +1,62 @@
 #!/usr/bin/python3
-from src.demo_problems import SwiftHohenberg
-from src.time_stepper import RungeKuttaFehlberg45
+from src.problem import Problem
+from src.time_stepper import RungeKutta4, RungeKuttaFehlberg45
 import os
 import shutil
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Pseudospectral implementation of the 1-dimensional Swift-Hohenberg Equation
+# equation, a nonlinear PDE
+# \partial t h &= (r - (kc^2 + \Delta)^2)h + v * h^2 - g * h^3
+class SwiftHohenberg(Problem):
+
+    def __init__(self, N, L):
+        super().__init__()
+        # parameters
+        self.r = -0.013
+        self.kc = 0.5
+        self.v = 0.41
+        self.g = 1
+        # space and fourier space
+        self.x = np.linspace(-L/2, L/2, N)
+        self.k = np.fft.rfftfreq(N, L / (2. * N * np.pi))
+        # initialize unknowns
+        self.u = 1 * np.cos(2 * np.pi * self.x / 10) * np.exp(-0.005 * self.x**2)
+
+
+    def rhs(self, u):
+        u_k = np.fft.rfft(u)
+        return np.fft.irfft((self.r - (self.kc**2 - self.k**2)**2) * u_k) + self.v * u**2 - self.g * u**3
+
+    def dealias(self, fraction=1./3.):
+        u_k = np.fft.rfft(self.u)
+        N = len(u_k)
+        u_k[-int(N*fraction):] = 0
+        self.u = np.fft.irfft(u_k)
+
 shutil.rmtree("out", ignore_errors=True)
 os.makedirs("out/img", exist_ok=True)
 
-she = SwiftHohenberg(512, 240)
-she.time_stepper = RungeKuttaFehlberg45()
-she.time_stepper.dt = 1e-3
-she.time_stepper.adaptive_timesteps = False
-she.time_stepper.error_dt = 1e1
+problem = SwiftHohenberg(512, 240)
+problem.time_stepper = RungeKuttaFehlberg45(dt=1e-3)
+problem.time_stepper.error_tolerance = 1e-1
 
 fig, ax = plt.subplots()
 plotevery = 500
 n = 0
 while True:
     if n % plotevery == 0:
-        ax.plot(she.x, she.u)
-        # u_k = np.fft.rfft(she.u)
-        # ax.plot(she.k, np.abs(u_k))
+        ax.plot(problem.x, problem.u)
+        # u_k = np.fft.rfft(problem.u)
+        # ax.plot(problem.k, np.abs(u_k))
         fig.savefig("out/img/{:05d}.svg".format(n//plotevery))
         ax.clear()
         print("Step #{:05d}".format(n//plotevery))
-        print("dt:   {:}".format(she.time_stepper.dt))
+        print("dt:   {:}".format(problem.time_stepper.dt))
     n += 1
-    she.time_step()
-    if np.max(she.u) > 1e12:
+    problem.time_step()
+    problem.dealias()
+    if np.max(problem.u) > 1e12:
         break
 
