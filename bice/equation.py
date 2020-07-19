73a5e90423ad52987b8666c533032409120cbb8e
simon123h <simon123h@users.noreply.github.com>
2020-07-19 20:56:02 +0200

    Add draft of the equation class

---
+++ bice/equation.py
@@ -0,0 +1,18 @@
+
+class Equation:
+
+    def __init__(self):
+        # The vector of unknowns
+        self.u = None
+        # Is the left-hand side (lhs) = du/dt (time evolution / Cauchy equation) or is it zero?
+        self.equals_dudt = True
+
+    # The (vector) dimension of the equation
+    @property
+    def dim(self):
+        return self.u.size
+
+    # Calculate the right-hand side of the equation 0 = rhs(u)
+    def rhs(self, u):
+        raise NotImplementedError(
+            "No right-hand side (rhs) implemented for this equation!")
