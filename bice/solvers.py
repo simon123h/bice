b0016ecbc146434f1e1935e823d920ee9553852b
simon123h <simon123h@users.noreply.github.com>
2020-07-20 21:29:43 +0200

    Unify the experimental solver-classes to single file

---
+++ bice/solvers.py
@@ -0,0 +1,35 @@
+import numpy as np
+import scipy.optimize
+import scipy.sparse
+
+# NOTE: these classes are not yet used at all, future work!
+
+
+class LinearSolver:
+
+    def solve(self, f, u):
+        raise NotImplementedError
+
+
+class NewtonSolver:
+
+    def solve(self, f, u):
+        return scipy.optimize.newton(f, u)
+
+
+class DirectEigenSolver:
+
+    def solve(self, A, k=None):
+        if k is None:
+            # if no number of values was specified, use a direct eigensolver for computing all eigenvalues
+            eigenvalues, eigenvectors = np.linalg.eig(A)
+        else:
+            # else: compute only the largest k eigenvalues with an iterative eigensolver
+            # this iterative eigensolver relies on ARPACK (Arnoldi method)
+            # TODO: optimize arguments for iterative eigensolver
+            eigenvalues, eigenvectors = scipy.sparse.linalg.eigs(A, k)
+        # sort by largest eigenvalue (largest real part)
+        idx = np.argsort(eigenvalues)[-1]
+        eigenvalues = eigenvalues[idx]
+        eigenvectors = eigenvectors[:, idx]
+        return (eigenvalues, eigenvectors)
