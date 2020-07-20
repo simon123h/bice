Fix jacobian approximation

bice/problem.py
@@ -2,6 +2,7 @@ from .time_steppers import RungeKutta4
 from .continuation_steppers import NaturalContinuation
 import numpy as np
 
+
 class Problem():
     """
     All algebraic problems inherit from the 'Problem' class.
@@ -32,7 +33,8 @@ class Problem():
 
     # Calculate the right-hand side of the system 0 = rhs(u)
     def rhs(self, u):
-        raise NotImplementedError("No right-hand side (rhs) implemented for this problem!")
+        raise NotImplementedError(
+            "No right-hand side (rhs) implemented for this problem!")
 
     # Calculate the Jacobian of the system J = d rhs(u) / du for the unknowns u.
     # 'eps' is the step size used for the central FD scheme
@@ -40,8 +42,12 @@ class Problem():
         # default implementation: calculate Jacobian with finite differences
         J = np.zeros([self.dim, self.dim], dtype=np.float)
         for i in range(self.dim):
-            f1 = self.rhs(u - eps)
-            f2 = self.rhs(u + eps)
+            u1 = u.copy()
+            u2 = u.copy()
+            u1[i] += eps
+            u2[i] -= eps
+            f1 = self.rhs(u1)
+            f2 = self.rhs(u2)
             J[:, i] = (f1 - f2) / (2 * eps)
         return J
 
@@ -50,9 +56,9 @@ class Problem():
         # perform timestep according to current scheme
         self.time_stepper.step(self)
 
-
     # Solve the equation rhs(u) = 0 for u with the assigned linear solver
     # Perform a parameter continuation step, w.r.t the parameter defined by
     # self.continuation_stepper.get_parameter/set_parameter
+
     def continuation_step(self):
         self.continuation_stepper.step(self)