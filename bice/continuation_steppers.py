68196375b27912ceb69629c81de252cab13806a4
simon123h <simon123h@users.noreply.github.com>
2020-07-20 15:11:59 +0200

    Enable step size adaption for arclength continuation

--- bice/continuation_steppers.py
+++ bice/continuation_steppers.py
@@ -50,15 +50,33 @@ class PseudoArclengthContinuation(ContinuationStepper):
 
     def __init__(self, ds=1e-3):
         super().__init__(ds)
+        # if the norm of the step in the newton loop is below this threshold, the method has converged
+        self.convergence_epsilon = 1e-10
+        # maximum number of newton iterations for solving
+        self.max_newton_iterations = 30
+        # should the step size be adapted while stepping?
+        self.adapt_stepsize = True
+        # the desired number of newton iterations for solving, step size is adapted if we over/undershoot this number
+        self.ndesired_newton_steps = 8
+        # the actual number of newton iterations taken in the last continuation step
+        self.nnewton_iter_taken = None
+        # ds 'penalty' factor when less nthan desired_newton_steps are performed
+        self.ds_decrease_factor = 0.5
+        # ds 'penalty' factor when more nthan desired_newton_steps are performed
+        self.ds_increase_factor = 1.05
+        # maximum step size
+        self.ds_max = 1e0
+        # minimum step size
+        self.ds_min = 1e-9
         # rescale the parameter constraint, for numerical stability
         # may be decreased for, e.g., very sharp folds
         self.constraint_scale = 1
 
     # perform continuation step
+
     def step(self, problem):
         p = problem.get_parameter()
         u = problem.u
-        count = 0
         N = u.size
         # TODO: use tangent vector from previous run?
         # build extended jacobian in (u, parameter)-space for tangent calculation
@@ -75,24 +93,20 @@ class PseudoArclengthContinuation(ContinuationStepper):
         zero = np.zeros(N+1)
         zero[N] = 1  # for solvability
         jac = np.concatenate((jac, zero.reshape((1, N+1))), axis=0)
-        # compute tangent by solving (jac)*tangent=0
+        # compute tangent by solving (jac)*tangent=0 and normalize
         tangent = np.linalg.solve(jac, zero)
-        # normalize
         tangent /= np.linalg.norm(tangent)
         # save old variables
-        u_old = u
+        u_old = u.copy()
         p_old = p
         # make initial guess: u -> u + ds * tangent
         u = u + self.ds * tangent[:N]
         p = p + self.ds * tangent[N]
-        # initial guess for the step in (u, p)-space
-        dU_ext = np.append(u - u_old, p - p_old)
 
-        # assemble the extended system: (jac_ext) * u_ext = rhs_ext
-        def f(u_ext):
+        converged = False
+        count = 0
+        while not converged and count < self.max_newton_iterations:
             # build extended jacobian in (vars, parameter)-space
-            u = u_ext[:N]
-            p = u_ext[N]
             problem.set_parameter(p)
             rhs = problem.rhs(u)
             jac = problem.jacobian(u)
@@ -101,22 +115,58 @@ class PseudoArclengthContinuation(ContinuationStepper):
             rhs_1 = problem.rhs(u)
             problem.set_parameter(p + eps)
             rhs_2 = problem.rhs(u)
+            problem.set_parameter(p)
             drhs_dp = (rhs_2 - rhs_1) / (2. * eps)
             jac_ext = np.concatenate((jac, drhs_dp.reshape((N, 1))), axis=1)
             # last row of extended jacobian: tangent vector
             jac_ext = np.concatenate(
                 (jac_ext, tangent.reshape((1, N+1))), axis=0)
             # extended rhs: model's rhs + arclength condition
-            rhs_ext = np.sum((u - u_old) * tangent[:N]) + (
-                p - p_old) * tangent[N] * self.constraint_scale - self.ds
+            rhs_ext = (u - u_old).dot(tangent[:N]) + (p - p_old) * \
+                tangent[N] * self.constraint_scale - self.ds
             rhs_ext = np.append(rhs, rhs_ext)
-            # solving (jac_ext) * dU_ext = rhs_ext for dU_ext will now give the new solution
-            return jac.dot(u_ext) - rhs_ext
+            # solving (jac_ext) * du_ext = rhs_ext for du_ext will now give the new solution
+            du_ext = np.linalg.solve(jac_ext, rhs_ext)
+            u = u - du_ext[:N]
+            p = p - du_ext[N]
+            # update counter and check for convergence
+            count += 1
+            converged = np.linalg.norm(du_ext) < self.convergence_epsilon
 
-        # solve it with a Newton solver
-        # TODO: detect if Newton solver failed and reject step
-        dU_ext = scipy.optimize.newton_krylov(f, dU_ext)
+        # update number of steps taken
+        self.nnewton_iter_taken = count
 
-        # assign the new values
-        problem.u = u_old + dU_ext[:N]
-        problem.set_parameter(p_old + dU_ext[N])
+        if converged:
+            # system converged to new solution
+            # assign the new values
+            problem.u = u
+            problem.set_parameter(p)
+        else:
+            # we didn't converge :-(
+            # reset to old values
+            problem.u = u_old
+            problem.set_parameter(p_old)
+            # TODO: not converged, throw error
+
+        # adjust sign of step size according to current continuation direction
+        # this is crucial for continuation of folds.
+        if p < p_old:
+            self.ds = -abs(self.ds)
+        else:
+            self.ds = abs(self.ds)
+
+        # adapt step size
+        if self.adapt_stepsize:
+            if count > self.ndesired_newton_steps and abs(self.ds) > self.ds_min:
+                # decrease step size
+                sign = -1 if self.ds < 0 else 1
+                self.ds = max(
+                    abs(self.ds)*self.ds_decrease_factor, self.ds_min)*sign
+                # redo continuation step
+                # self.u = u_old
+                # problem.set_parameter(p_old)
+                # self.step(problem)
+            elif count < self.ndesired_newton_steps:
+                sign = -1 if self.ds < 0 else 1
+                self.ds = min(
+                    abs(self.ds)*self.ds_increase_factor, self.ds_max)*sign
