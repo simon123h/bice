dc3f69f5d7141731398184c0dd03af9df9b43fbf
simon123h <simon123h@users.noreply.github.com>
2020-07-21 16:08:37 +0200

    Unify plot function

--- demos/SHE/she.py
+++ demos/SHE/she.py
@@ -34,6 +34,8 @@ class SwiftHohenberg(Problem):
         self.time_stepper = RungeKuttaFehlberg45(dt=1e-3)
         self.time_stepper.error_tolerance = 1e-7
         self.time_stepper.dt = 1e-3
+        # plotting
+        self.plotID = 0
 
     def rhs(self, u):
         u_k = np.fft.rfft(u)
@@ -52,6 +54,44 @@ class SwiftHohenberg(Problem):
     def set_continuation_parameter(self, v):
         self.r = v
 
+    # plot everything
+    def plot(self, fig, ax, sol=None):
+        ax[0, 0].plot(self.x, self.u)
+        ax[0, 0].set_xlabel("x")
+        ax[0, 0].set_ylabel("solution u(x,t)")
+        ax[1, 0].plot(self.k, np.abs(np.fft.rfft(self.u)))
+        ax[1, 0].set_xlim((0, self.k[-1]/2))
+        ax[1, 0].set_xlabel("k")
+        ax[1, 0].set_ylabel("fourier spectrum u(k,t)")
+        for branch in self.bifurcation_diagram.branches:
+            r, norm = branch.data()
+            ax[0, 1].plot(r, norm, "--", color="C0")
+            r, norm = branch.data(only="stable")
+            ax[0, 1].plot(r, norm, color="C0")
+            r, norm = branch.data(only="bifurcations")
+            ax[0, 1].plot(r, norm, "*", color="C2")
+        ax[0, 1].plot(np.nan, np.nan, "*", color="C2", label="bifurcations")
+        ax[0, 1].plot(self.r, self.norm(),
+                      "x", label="current point", color="black")
+        ax[0, 1].set_xlabel("parameter r")
+        ax[0, 1].set_ylabel("L2-norm")
+        ax[0, 1].legend()
+        if sol:
+            ev_re = np.real(sol.eigenvalues[:20])
+            ev_re_n = np.ma.masked_where(
+                ev_re > self.eigval_zero_tolerance, ev_re)
+            ev_re_p = np.ma.masked_where(
+                ev_re <= self.eigval_zero_tolerance, ev_re)
+            ax[1, 1].plot(ev_re_n, "o", color="C0", label="Re < 0")
+            ax[1, 1].plot(ev_re_p, "o", color="C1", label="Re > 0")
+            ax[1, 1].axhline(0, color="gray")
+            ax[1, 1].legend()
+            ax[1, 1].set_ylabel("eigenvalues")
+        fig.savefig("out/img/{:05d}.svg".format(self.plotID))
+        self.plotID += 1
+        for a in ax.flatten():
+            a.clear()
+
 
 # create output folder
 shutil.rmtree("out", ignore_errors=True)
@@ -64,26 +104,12 @@ problem = SwiftHohenberg(N=512, L=240)
 fig, ax = plt.subplots(2, 2, figsize=(16, 9))
 n = 0
 plotevery = 1000
-plotID = 0
 dudtnorm = 1
 if not os.path.exists("initial_state.dat"):
     while dudtnorm > 1e-5:
         # plot
         if n % plotevery == 0:
-            ax[0, 0].plot(problem.x, problem.u)
-            ax[0, 0].set_xlabel("x")
-            ax[0, 0].set_ylabel("solution u(x,t)")
-            ax[1, 0].plot(problem.k, np.abs(np.fft.rfft(problem.u)))
-            ax[1, 0].set_xlabel("k")
-            ax[1, 0].set_ylabel("fourier spectrum u(k,t)")
-            ax[0, 1].plot(problem.get_continuation_parameter(),
-                          problem.norm(), label="current point", marker="x")
-            ax[0, 1].set_xlabel("parameter r")
-            ax[0, 1].set_ylabel("L2-norm")
-            fig.savefig("out/img/{:05d}.svg".format(plotID))
-            for a in ax:
-                a.clear()
-            plotID += 1
+            problem.plot(fig, ax)
             dudtnorm = np.linalg.norm(problem.rhs(problem.u))
             print("step #: {:}".format(n))
             print("time:   {:}".format(problem.time))
@@ -107,50 +133,13 @@ problem.continuation_stepper.ds = 1e-2
 problem.continuation_stepper.ndesired_newton_steps = 3
 problem.continuation_stepper.always_check_eigenvalues = True
 
-# lists for bifurcation diagram
-norms = []
-rs = []
-
 n = 0
-plotevery = 2
+plotevery = 3
 while problem.r < 1:
     # perform continuation step
     sol = problem.continuation_step()
     n += 1
-    print("step #:", n)
-    print("ds:    ", problem.continuation_stepper.ds)
+    print("step #:", n, " ds:", problem.continuation_stepper.ds)
     # plot
     if n % plotevery == 0:
-        ax[0, 0].plot(problem.x, problem.u)
-        ax[0, 0].set_xlabel("x")
-        ax[0, 0].set_ylabel("solution u(x,t)")
-        ax[1, 0].plot(problem.k, np.abs(np.fft.rfft(problem.u)))
-        ax[1, 0].set_xlim((0, problem.k[-1]/2))
-        ax[1, 0].set_xlabel("k")
-        ax[1, 0].set_ylabel("fourier spectrum u(k,t)")
-        for branch in problem.bifurcation_diagram.branches:
-            r, norm = branch.data()
-            ax[0, 1].plot(r, norm, "--", color="C0")
-            r, norm = branch.data(only="stable")
-            ax[0, 1].plot(r, norm, color="C0")
-            r, norm = branch.data(only="bifurcations")
-            ax[0, 1].plot(r, norm, "*", color="C2")
-        ax[0, 1].plot(problem.r, problem.norm(),
-                      "x", label="current point", color="black")
-        ax[0, 1].set_xlabel("parameter r")
-        ax[0, 1].set_ylabel("L2-norm")
-        ax[0, 1].legend()
-        ev_re = np.real(sol.eigenvalues[:20])
-        ev_re_n = np.ma.masked_where(
-            ev_re > problem.eigval_zero_tolerance, ev_re)
-        ev_re_p = np.ma.masked_where(
-            ev_re <= problem.eigval_zero_tolerance, ev_re)
-        ax[1, 1].plot(ev_re_n, "o", color="C0", label="Re < 0")
-        ax[1, 1].plot(ev_re_p, "o", color="C1", label="Re > 0")
-        ax[1, 1].axhline(0, color="gray")
-        ax[1, 1].legend()
-        ax[1, 1].set_ylabel("eigenvalues")
-        fig.savefig("out/img/{:05d}.svg".format(plotID))
-        for a in ax.flatten():
-            a.clear()
-        plotID += 1
+        problem.plot(fig, ax, sol)
