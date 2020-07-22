8d859e5f4c25fa5720d1f994ad14867264ba3f12
simon123h <simon123h@users.noreply.github.com>
2020-07-22 12:05:25 +0200

    Add drafts for constraints as Equation objects

--- bice/equation.py
+++ bice/equation.py
@@ -1,5 +1,7 @@
+import numpy as np
 
 # NOTE: this class is not yet used at all, future work!
+
 
 class Equation:
 
@@ -18,3 +20,37 @@ class Equation:
     def rhs(self, u):
         raise NotImplementedError(
             "No right-hand side (rhs) implemented for this equation!")
+
+    # The mass matrix determines the linear relation of the rhs to the temporal derivatives dudt
+    def mass_matrix(self):
+        # default case: assume the identity matrix I
+        return np.eye(self.dim)
+
+
+class VolumeConstraint(Equation):
+
+    def __init__(self, reference_equation):
+        super().__init__()
+        self.ref_eq = reference_equation
+
+    def rhs(self, u):
+        # calculate the difference in volumes
+        # between current and previous unknowns of the
+        # reference equation
+        return np.trapz(u-self.ref_eq.u, self.ref_eq.x)
+
+    # TODO: add mass_matrix
+
+class TranslationConstraint(Equation):
+
+    def __init__(self, reference_equation):
+        super().__init__()
+        self.ref_eq = reference_equation
+
+    def rhs(self, u):
+        # calculate the difference in center of masses
+        # between current and previous unknowns of the
+        # reference equation
+        return np.dot(self.ref_eq.x, u-self.ref_eq.u)
+
+    # TODO: add mass_matrix
