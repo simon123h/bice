00e6ae310c43cad78b2761befc8fc41fef736a84
simon123h <simon123h@users.noreply.github.com>
2020-07-26 18:58:54 +0200

    Revert the CH demo to normal

--- demos/CHE/ch2d.py
+++ demos/CHE/ch2d.py
@@ -35,22 +35,15 @@ class CahnHilliardEquation(Equation):
     # definition of the CHE (right-hand side)
     @profile
     def rhs(self, u):
+        # TODO: can we make this any faster?
         N0 = u.size
         u2 = u.reshape((self.x[0].size, self.x[1].size))
-        # u_k = np.fft.fft2(u)
-        # u3_k = np.fft.fft2(u**3)
-        # linear = -self.D * (self.ksquare * self.ksquare +
-        #                     self.e * self.ksquare)
-
-        u_k = np.fft.fft2(u2)  # Fouriertransformation
+        u_k = np.fft.fft2(u2)
         u3_k = np.fft.fft2(u2*u2*u2)
         result_k = (-self.D*self.ksquare*self.ksquare +
                     self.ksquare)*u_k - self.ksquare*u3_k
         result = np.fft.ifft2(result_k).real
-
         return result.reshape(u.size)
-
-        # return np.fft.ifft2(linear*u_k - self.ksquare*u3_k).real.reshape(u.size)
 
 
 class CahnHilliardProblem(Problem):
@@ -76,56 +69,50 @@ os.makedirs("out/img", exist_ok=True)
 problem = CahnHilliardProblem(N=128, L=128)
 
 # create figure
-# fig, ax = plt.subplots(2, 2, figsize=(16, 9))
+fig, ax = plt.subplots(2, 2, figsize=(16, 9))
 plotID = 0
 
 # time-stepping
 n = 0
-plotevery = 1000
+plotevery = 200
 dudtnorm = 1
-# mx, my = np.meshgrid(problem.che.x[0], problem.che.x[1])
-
-# if not os.path.exists("initial_state2.dat"):
+mx, my = np.meshgrid(problem.che.x[0], problem.che.x[1])
 
 Profiler.start()
 
-for i in range(500):
-    problem.time_step()
+if not os.path.exists("initial_state.dat"):
+    while dudtnorm > 1e-6:
+        # plot
+        if n % plotevery == 0:
+            plt.cla()
+            plt.pcolormesh(mx, my, problem.che.u.reshape(
+                (problem.che.x[0].size, problem.che.x[1].size)), edgecolors='face')
+            plt.colorbar()
+            plt.savefig("out/img/{:05d}.svg".format(plotID))
+            plt.close()
+            # problem.plot(ax)
+            # fig.savefig("out/img/{:05d}.svg".format(plotID))
+            plotID += 1
+            print("step #: {:}".format(n))
+            print("dt:     {:}".format(problem.time_stepper.dt))
+            print("|dudt|: {:}".format(dudtnorm))
+
+        n += 1
+        # perform timestep
+        problem.time_step()
+        # calculate the new norm
+        dudtnorm = np.linalg.norm(problem.rhs(problem.u))
+        # catch divergent solutions
+        if np.max(problem.u) > 1e12:
+            break
+
+    # save the state, so we can reload it later
+    problem.save("initial_state.dat")
+else:
+    # load the initial state
+    problem.load("initial_state.dat")
 
 Profiler.print_summary()
-
-# while dudtnorm > 1e-6:
-# # for i in range(500):
-#     # plot
-#     if n % plotevery == 0:
-#         plt.cla()
-#         plt.pcolormesh(mx, my, problem.che.u.reshape(
-#             (problem.che.x[0].size, problem.che.x[1].size)), edgecolors='face')
-#         plt.colorbar()
-#         plt.savefig("out/img/{:05d}.svg".format(plotID))
-#         plt.close()
-#         # problem.plot(ax)
-#         # fig.savefig("out/img/{:05d}.svg".format(plotID))
-#         plotID += 1
-#         print("step #: {:}".format(n))
-#         print("dt:     {:}".format(problem.time_stepper.dt))
-#         print("|dudt|: {:}".format(dudtnorm))
-
-#     n += 1
-#     # perform timestep
-#     problem.time_step()
-#     # calculate the new norm
-#     dudtnorm = np.linalg.norm(problem.rhs(problem.u))
-#     # catch divergent solutions
-#     if np.max(problem.u) > 1e12:
-#         break
-
-
-#     # save the state, so we can reload it later
-#     problem.save("initial_state.dat")
-# else:
-#     # load the initial state
-#     problem.load("initial_state.dat")
 
 # # start parameter continuation
 # problem.continuation_stepper.ds = 1e-3
