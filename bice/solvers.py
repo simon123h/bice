c2ae71e3204f64aa9bf1c8d7bed04b331393117e
simon123h <simon123h@users.noreply.github.com>
2020-07-27 12:17:18 +0200

    Optimize the iterative eigensolver

--- bice/solvers.py
+++ bice/solvers.py
@@ -18,17 +18,24 @@ class NewtonSolver:
 
 class EigenSolver:
 
-    def solve(self, A, M=None, k=None):
-        # TODO: find an optimal solver
+    def solve(self, A, M=None, k=None, sigma=1):
         if k is None:
             # if no number of values was specified, use a direct eigensolver for computing all eigenvalues
             eigenvalues, eigenvectors = scipy.linalg.eig(A, M)
         else:
             # else: compute only the largest k eigenvalues with an iterative eigensolver
             # this iterative eigensolver relies on ARPACK (Arnoldi method)
-            # which = 'LR' --> largest real part first
-            # TODO: optimize arguments for iterative eigensolver
-            eigenvalues, eigenvectors = scipy.sparse.linalg.eigs(A, k, M=M, which='LM')
+            # A: matrix of which we compute the eigenvalues
+            # k: number of eigenvalues to compute in iterative method
+            # M: mass matrix for generized eigenproblem A*x=w*M*x
+            # which: order of eigenvalues to compute ('LM' = largest magnitude is default and the fastest)
+            # sigma: Find eigenvalues near sigma using shift-invert mode.
+            # v0: Starting vector for iteration. Default: random.
+            #     This may not be deterministic, since it is random! For this reason, pde2path uses a [1,...,1]-vector
+            # For more info, see the documentation:
+            # https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.eigs.html
+            eigenvalues, eigenvectors = scipy.sparse.linalg.eigs(
+                A, k=k, M=M, sigma=sigma, which='LM')
         # sort by largest eigenvalue (largest real part) and filter infinite eigenvalues
         idx = np.argsort(eigenvalues)[::-1]
         idx = idx[np.isfinite(eigenvalues[idx])]
