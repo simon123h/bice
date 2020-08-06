bbcdeb1602e3841f77c160a2a236d310e250b3c9
simon123h <simon123h@users.noreply.github.com>
2020-08-06 21:37:36 +0200

    Try to implement semi-analytical Jacobian for BifurcationConstraint, but we still need N evaluations of the Jacobian :-(

--- bice/bifurcations.py
+++ bice/bifurcations.py
@@ -31,24 +31,14 @@ class BifurcationConstraint(Equation):
         # get the value of the current and the previous null-eigenvector phi
         phi = u[self.idx][:-1]
         phi_old = self.u[:-1]
-        # apply the given value of the free parameter
-        param_obj, param_name = tuple(self.free_parameter)
-        setattr(param_obj, param_name, u[self.idx][-1])
-        # calculate the original Jacobian of the problem by disabling self
-        self.__disabled = True
-        Gu = self.problem.jacobian(u)
-        self.__disabled = False
-        # remove those columns/rows of the Jacobian that belong to self,
-        # so we are left with the original (unextended) Jacobian
-        Gu = np.delete(np.delete(Gu, self.idx, axis=0), self.idx, axis=1)
+        # calculate the original Jacobian of the problem
+        Gu = self.original_jacobian(u)
         # check for mismatch in dimension
         if Gu.shape != (phi.size, phi.size):
             raise Exception("It seems that the dimension of the problem does not "
                             "match the dimension of the null-eigenvector phi in "
                             "the BifurcationConstraint. Did your problem change "
                             "since you imposed the constraint?")
-        # reset the value of the free parameter
-        setattr(param_obj, param_name, self.u[-1])
         # calculate the residuals
         res = np.zeros((u.size))
         res1 = np.matmul(Gu, phi)
@@ -60,33 +50,84 @@ class BifurcationConstraint(Equation):
         # if the constraint is disabled, no Jacobian will be calculated
         if self.__disabled:
             return 0
-        # apply the given value of the free parameter
-        param_obj, param_name = tuple(self.free_parameter)
-        setattr(param_obj, param_name, u[self.idx][-1])
         # pass Jacobian calculation to the FD method of the parent Equation class
-        J = Equation.jacobian(self, u)
-        # as the default implementation does not respect that the free parameter is
-        # an unknown, add the corresponding parameter derivative to the Jacobian.
-        # the constraint's own rhs does respect it, though. --> disable it
-        self.__disabled = True
+        # NOTE: this is what takes so long, since it evaluates the rhs 2N+1 times
+        #       and the rhs evaluates the original Jacobian... :-/
+        return Equation.jacobian(self, u)
+
+        # Alternative: quasi-analytical calculation...
+
+        # empty result matrix
+        J = np.zeros((u.size, u.size))
+        r"""
+            /   d(rhs_orig)/d(u_orig) [NxN]  |  d(rhs_orig)/d(phi) = 0  [NxN]  |                 \
+            |   -----------------------------|---------------------------------|                 |
+        J = |   d(res1)/d(u_orig)     [NxN]  |  d(res1)/d(phi) = Gu     [NxN]  | d(rhs)/d(param) |
+            |   -----------------------------|---------------------------------|   [(2N+1)x1]    |
+            \   d(res2)/d(u_orig) = 0 [1xN]  | d(res2)/d(phi) = phi_old [1xN]  |                 /
+        """
+
+        # upper left block: d(rhs_orig)/d(u_orig) will be assembled by their respective equations
+
+        # get phi and phi_old
+        phi = u[self.idx][:-1]
+        phi_old = self.u[:-1]
+        # calculate the original Jacobian of the problem
+        Gu = self.original_jacobian(u)
+
+        # lower left sub-block: d(res1)/d(u_orig), calculate with FD
+        eps = 1e-10
+        u1 = u.copy()
+        for i in range(phi.size):
+            k = u1[i]
+            u1[i] = k + eps
+            # calculate the original Jacobian of the problem
+            # NOTE: oookay... here we'd also need N times evaluation of the Jacobian... that's slow as well
+            Gu1 = self.original_jacobian(u1)
+            # NOTE: the following line is not general, wrt. self.idx
+            J[:, i] = np.matmul(Gu1-Gu, phi) / eps
+            # .... I'm giving up at this point!
+
+        # lower right sub-block: d(res1)/d(phi) = Gu
+        res1_slice = slice(self.idx.start, self.idx.stop-1)
+        J[res1_slice, res1_slice] = Gu
+
+        # last column: d(rhs)/d(param), calculate with FD
         f0 = self.problem.rhs(u)
         # deviate the free parameter value
-        eps = 1e-10
-        setattr(param_obj, param_name, u[self.idx][-1] + eps)
+        param_val = u[self.idx][-1]
+        u[self.idx][-1] += eps
         # calculate new residuals
         f1 = self.problem.rhs(u)
+        # reset the value of the free parameter
+        u[self.idx][-1] = param_val
         # add FD parameter derivative to Jacobian
-        J = J.T
-        J[self.idx.stop-1] += (f1 - f0) / eps
-        # re-enable the constraint
-        self.__disabled = False
-        # reset the value of the free parameter and return the Jacobian
-        setattr(param_obj, param_name, self.u[-1])
-        return J.T
+        J[:, self.idx.stop-1] = (f1 - f0) / eps
+
+        # last row: d(res2)/du = ([0]*N, phi_old, 0)
+        J[self.idx.stop -
+            1] = np.concatenate((np.zeros(phi_old.size), phi_old, np.array([0])))
+
+        return J
 
     def mass_matrix(self):
         # couples to no time-derivatives
         return 0
+
+    # calculate the original (unextended) Jacobian of the problem
+    def original_jacobian(self, u):
+        # disable the null-space equations
+        self.__disabled = True
+        Gu = self.problem.jacobian(u)
+        self.__disabled = False
+        # remove those columns/rows of the Jacobian that belong to self,
+        # so we are left with the original (unextended) Jacobian
+        return np.delete(np.delete(Gu, self.idx, axis=0), self.idx, axis=1)
+
+    def actions_before_evaluation(self, u):
+        # write the free parameter back from the given unknowns
+        param_obj, param_name = tuple(self.free_parameter)
+        setattr(param_obj, param_name, u[-1])
 
     def actions_after_newton_solve(self):
         # write the free parameter back from the unknowns
