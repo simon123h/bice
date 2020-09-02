b6aa9701602d31b23c655fd5d29ce5a0d22197b5
simon123h <simon123h@users.noreply.github.com>
2020-09-02 21:46:10 +0200

    Rewrite the Element classes to use local coordinate mapping

--- bice/fem.py
+++ bice/fem.py
@@ -53,7 +53,8 @@ class MyFiniteElementEquation(Equation):
             # generate x
             Nx, Ny = N
             Lx, Ly = L
-            self.x = [np.linspace(0, Lx, Nx, endpoint=True), np.linspace(0, Ly, Ny, endpoint=True)]
+            self.x = [np.linspace(0, Lx, Nx, endpoint=True),
+                      np.linspace(0, Ly, Ny, endpoint=True)]
             # add equidistant nodes
             for i in range(Nx):
                 for j in range(Ny):
@@ -113,6 +114,7 @@ class MyFiniteElementEquation(Equation):
                             self.nabla[d][ni.index, nj.index] += dshape[d][i] * \
                                 test[j] * weight
 
+
 class Node:
 
     def __init__(self, x):
@@ -126,11 +128,11 @@ class Element:
         self.nodes = nodes
 
     # returns a list of all shape functions of this element
-    def shape(self, x):
+    def shape(self, s):
         pass
 
     # returns a list of all shape function derivatives of this element
-    def dshape(self, x):
+    def dshape(self, s):
         pass
 
     # returns a list of all test functions of this element
@@ -141,6 +143,8 @@ class Element:
     def dtest(self, x):
         return self.dshape(x)
 
+# one-dimensional elements with linear shape functions
+
 
 class Element1d(Element):
 
@@ -148,68 +152,118 @@ class Element1d(Element):
         super().__init__(nodes)
         self.x0 = nodes[0].x[0]
         self.x1 = nodes[1].x[0]
-        self.dx = self.x1 - self.x0
+        # transformation matrix from local to global coordinates
+        self.transformation_matrix = np.array([
+            [self.x1-self.x0]
+        ])
+        # corresponding determinant of transformation
+        self.transformation_det = self.x1-self.x0
         # exact polynomial integration using Gaussian quadrature
         # see: https://de.wikipedia.org/wiki/Gau%C3%9F-Quadratur#Gau%C3%9F-Legendre-Integration
-        a = np.array([(self.x0 + self.x1) / 2])
-        b = np.array([np.sqrt(1./3.) * self.dx / 2])
-        w = self.dx / 2
-        self.integration_points = [(a + b, w), (a - b, w)]
+        # and: https://link.springer.com/content/pdf/bbm%3A978-3-540-32609-0%2F1.pdf
+        a = 1./2.
+        b = np.sqrt(1./3.) / 2
+        w = self.transformation_det / 2
+        self.integration_points = [
+            (np.array([a-b]), w),
+            (np.array([a+b]), w)
+        ]
 
-    # 1d linear shape functions
-    def shape(self, x):
-        s = (x[0] - self.x0) / self.dx  # local coordinate
+    # 1d linear triangle shape functions
+    def shape(self, s):
+        s = s[0]
         return [1-s, s]
 
     # 1d linear shape functions within the element
-    def dshape(self, x):
-        ds_dx = 1 / self.dx  # local coordinate derivative
-        return [[-ds_dx, ds_dx]]
+    def dshape(self, s):
+        return [[-1, 1]]
 
 
-class Element2d(Element):
+# two-dimensional triangular elements with linear shape functions
+class TElement2d(Element):
 
     def __init__(self, nodes):
         super().__init__(nodes)
-        self.x0 = nodes[0].x[0]
-        self.x1 = nodes[1].x[0]
-        self.y0 = nodes[0].x[0]
-        self.y1 = nodes[1].x[0]
-        self.dx = self.x1 - self.x0
-        self.dy = self.y1 - self.x0
+        self.n0 = nodes[0]
+        self.n1 = nodes[1]
+        self.n2 = nodes[2]
+        self.x0 = self.n0.x[0]
+        self.y0 = self.n0.x[1]
+        self.x1 = self.n1.x[0]
+        self.y1 = self.n1.x[1]
+        self.x2 = self.n2.x[0]
+        self.y2 = self.n2.x[1]
+        # transformation matrix from local to global coordinates
+        self.transformation_matrix = np.array([
+            [self.x1-self.x0, self.x2-self.x0],
+            [self.y1-self.y0, self.y2-self.y0]
+        ])
+        # corresponding determinant of transformation
+        self.transformation_det = (
+            self.x1-self.x0)*(self.y2-self.y0)-(self.x2-self.x0)*(self.y1-self.y0)
         # exact polynomial integration using Gaussian quadrature
         # see: https://de.wikipedia.org/wiki/Gau%C3%9F-Quadratur#Gau%C3%9F-Legendre-Integration
-        ax = (self.x0 + self.x1) / 2
-        bx = np.sqrt(1./3.) * self.dx / 2
-        ay = (self.y0 + self.y1) / 2
-        by = np.sqrt(1./3.) * self.dy / 2
-        w = self.dx / 2
+        # and: https://link.springer.com/content/pdf/bbm%3A978-3-540-32609-0%2F1.pdf
+        w = self.transformation_det / 6
         self.integration_points = [
-            (np.array([ax-bx, ay-by]), w),
-            (np.array([ax+bx, ay-by]), w),
-            (np.array([ax-bx, ay+by]), w),
-            (np.array([ax+bx, ay+by]), w)
+            (np.array([0.5, 0.5]), w),
+            (np.array([0.0, 0.5]), w),
+            (np.array([0.5, 0.0]), w)
         ]
 
-    # 2d linear shape functions
-    def shape(self, x):
-        # local coordinates
-        sx = (x[0] - self.x0) / self.dx
-        sy = (x[1] - self.y0) / self.dy
-        # again, note the counter-clockwise order of the nodes
+    # 2d linear triangle shape functions
+    def shape(self, s):
+        sx, sy = s
+        # three shape functions, counter-clockwise order of nodes
+        return [1-sx-sy, sx, sy]
+
+    # 2d linear shape functions within the element
+    def dshape(self, s):
+        # (dshape_dsx, dshape_dsy)
+        return [
+            [-1, 1, 0],
+            [-1, 0, 1]
+        ]
+
+# two-dimensional rectangular elements with linear shape functions
+class QElement2d(Element):
+
+    def __init__(self, nodes):
+        super().__init__(nodes)
+        self.x0 = self.nodes[0].x[0]
+        self.y0 = self.nodes[0].x[1]
+        self.x1 = self.nodes[2].x[0]
+        self.y1 = self.nodes[2].x[1]
+        # transformation matrix from local to global coordinates
+        self.transformation_matrix = np.array([
+            [self.x1-self.x0, 0],
+            [0, self.y1-self.y0]
+        ])
+        # corresponding determinant of transformation
+        self.transformation_det = (self.x1-self.x0)*(self.y1-self.y0)
+        # exact polynomial integration using Gaussian quadrature
+        # see: https://de.wikipedia.org/wiki/Gau%C3%9F-Quadratur#Gau%C3%9F-Legendre-Integration
+        # and: https://link.springer.com/content/pdf/bbm%3A978-3-540-32609-0%2F1.pdf
+        a = 1./2.
+        b = np.sqrt(1./3.) / 2
+        w = self.transformation_det / 2
+        self.integration_points = [
+            (np.array([a-b, a-b]), w),
+            (np.array([a+b, a-b]), w),
+            (np.array([a-b, a+b]), w),
+            (np.array([a+b, a+b]), w)
+        ]
+
+    # 2d linear rectangle shape functions
+    def shape(self, s):
+        sx, sy = s
+        # four shape functions, counter-clockwise order of nodes
         return [(1-sx)*(1-sy), (1-sx)*sy, sx*sy, sx*(1-sy)]
 
     # 2d linear shape functions within the element
-    def dshape(self, x):
-        # local coordinates
-        sx = (x[0] - self.x0) / self.dx
-        sy = (x[1] - self.y0) / self.dy
-        # local coordinate derivatives
-        dsx_dx = 1 / self.dx
-        dsy_dy = 1 / self.dy
-        # derivatives
-        # again, note the counter-clockwise order of the nodes
+    def dshape(self, s):
+        sx, sy = s
         return [
-            [-dsx_dx*(1-sy), -dsx_dx*sy, dsx_dx*sy, dsx_dx*(1-sy)],
-            [(1-sx)*dsy_dy, (1-sx)*dsy_dy, sx*dsy_dy, -sx*dsy_dy]
+            [sy-1, -sy, sy, 1-sy],
+            [sx-1, 1-sx, sx, -sx]
         ]
