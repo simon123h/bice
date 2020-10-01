0128ae1cdccb77e1e441ce8a54006f7a78a58936
simon123h <simon123h@users.noreply.github.com>
2020-10-01 16:33:09 +0200

    Refactor MyNewtonSolver

--- bice/solvers.py
+++ bice/solvers.py
@@ -11,18 +11,15 @@ class MyNewtonSolver:
         self.iteration_count = 0
 
     def solve(self, f, u, J):
-        converged = False
         self.iteration_count = 0
-        while not converged and self.iteration_count < self.max_newton_iterations:
+        while self.iteration_count < self.max_newton_iterations:
             du = np.linalg.solve(J(u), f(u))
             u -= du
-            # update counter and check for convergence
             self.iteration_count += 1
-            converged = np.linalg.norm(du) < self.convergence_tolerance
-        # system converged to new solution, return it
-        if converged:
-            return u
-        # else, we didn't converge, throw an error
+            # if system converged to new solution, return solution
+            if np.linalg.norm(du) < self.convergence_tolerance:
+                return u
+        # if we didn't converge, throw an error
         raise np.linalg.LinAlgError(
             "Newton solver did not converge after", self.iteration_count, "iterations!")
 
