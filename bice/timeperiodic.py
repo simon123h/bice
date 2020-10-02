Fix time-derivative in timeperiodic continuation

bice/timeperiodic.py
@@ -33,7 +33,7 @@ class TimePeriodicOrbitHandler(Equation):
 
     # return the time derivative for a given list u's at each timestep using central differences
     def dudt(self, eq_u):
-        du = (np.roll(eq_u, 1) - np.roll(eq_u, -1)) * 0.5
+        du = (np.roll(eq_u, -1, axis=0) - np.roll(eq_u, 1, axis=0)) * 0.5
         return (du.T / self.dt).T
 
     # calculate the rhs of the full system of equations

demos/LVE/lve.py
@@ -63,10 +63,10 @@ print("time   :", end - start)
 
 
 data = np.array(data)
-plt.plot(data[:, 0], data[:, 1])
+plt.plot(data[:, 0], data[:, 1], label="time-stepping solution")
 # plt.show()
 
-T = 6.38
+T = 6.34
 Nt = 100
 
 problem.time_stepper.dt = T / Nt
@@ -88,12 +88,13 @@ problem.add_equation(tpoh)
 
 print("newton solve")
 x, y = tpoh.u[:-1].reshape((Nt, 2)).T
-plt.plot(y, x, "x", color="green")
+plt.plot(y, x, "x", color="green", label="continuation initial condition")
 r = problem.rhs(problem.u)
 print(r)
 problem.newton_solve()
 T = tpoh.u[-1]
 print("T =", T)
 x, y = tpoh.u[:-1].reshape((Nt, 2)).T
-plt.plot(y, x, "x", color="orange")
+plt.plot(y, x, "x", color="orange", label="continuation solution")
+plt.legend()
 plt.show()