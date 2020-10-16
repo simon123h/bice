406caf198a00b813875b2e0be58161ded64bb77b
simon123h <simon123h@users.noreply.github.com>
2020-10-16 19:10:25 +0200

    Rewrite NE1d to real space, so that continuation works

--- demos/NE/ne1d.py
+++ demos/NE/ne1d.py
@@ -22,11 +22,10 @@ class NikolaevskiyEquation(PseudospectralEquation):
         # make sure N is even
         N = int(np.floor(N/2)*2)
         # we have only a single variable h
-        self.shape = (N//2+1,)
-        # self.shape = (N2+1,)
+        self.shape = (N,)
         # parameters
-        self.r = 0.5 # drive
-        self.m = 10 # characteristic system length
+        self.r = 0.5  # drive
+        self.m = 10  # characteristic system length
         # space and fourier space
         L = 1
         self.x = [np.linspace(0, L, N)]
@@ -34,8 +33,7 @@ class NikolaevskiyEquation(PseudospectralEquation):
         self.ksquare = self.k[0]**2
         # initial condition
         self.u = 2*(np.random.rand(N)-0.5) * 1e-5
-        self.u = np.fft.rfft(self.u)
-
+        # self.u = np.fft.rfft(self.u)
 
     # characteristic length scale
     @property
@@ -49,24 +47,25 @@ class NikolaevskiyEquation(PseudospectralEquation):
         # include length scale in spatial derivatives
         k = self.k[0] / L
         ksq = self.ksquare / L**2
+        # fourier transform
+        u_k = np.fft.rfft(u)
         # calculate linear part (in fourier space)
-        lin = ksq * (self.r - (1-ksq)**2) * u
+        lin = ksq * (self.r - (1-ksq)**2) * u_k
         # calculate nonlinear part (in real space)
-        nonlin = - 0.5 * np.fft.irfft(1j * k * u)**2
+        nonlin = - 0.5 * np.fft.irfft(1j * k * u_k)**2
         # sum up and return
-        return lin + np.fft.rfft(nonlin)
+        return np.fft.irfft(lin) + nonlin
 
     def plot(self, ax):
         ax.set_xlabel("x")
         ax.set_ylabel("h(x,t)")
         L = self.L0 * self.m
-        ax.plot(self.x[0] * L, np.fft.irfft(self.u))
-
+        ax.plot(self.x[0] * L, self.u)
 
 
 class NikolaevskiyProblem(Problem):
 
-    def __init__(self, N, L):
+    def __init__(self, N):
         super().__init__()
         # Add the Nikolaevskiy equation to the problem
         self.ne = NikolaevskiyEquation(N)
@@ -74,17 +73,19 @@ class NikolaevskiyProblem(Problem):
         # initialize time stepper
         # self.time_stepper = RungeKutta4(dt=1e-7)
         # self.time_stepper = RungeKuttaFehlberg45(dt=1e-7, error_tolerance=1e-4)
-        # self.time_stepper = BDF2(dt=1e-1)
-        self.time_stepper = BDF(self)
+        # self.time_stepper = BDF2(dt=1e-3)
+        self.time_stepper = BDF(self, dt_max=1e-1)
         # assign the continuation parameter
         self.continuation_parameter = (self.ne, "r")
 
     # set higher modes to null, for numerical stability
     def dealias(self, fraction=1./2.):
-        N = len(self.ne.u)
+        u_k = np.fft.rfft(self.ne.u)
+        N = len(u_k)
         k = int(N*fraction)
-        # self.ne.u[k+1:-k] = 0
-        self.ne.u[0] = 0
+        u_k[k+1:] = 0
+        u_k[0] = 0
+        self.ne.u = np.fft.irfft(u_k)
 
 
 # create output folder
@@ -92,7 +93,9 @@ shutil.rmtree("out", ignore_errors=True)
 os.makedirs("out/img", exist_ok=True)
 
 # create problem
-problem = NikolaevskiyProblem(N=256, L=48)
+problem = NikolaevskiyProblem(N=256)
+problem.ne.r = 0.5
+problem.ne.m = 1.1
 
 # create figure
 fig, ax = plt.subplots(1, 1, figsize=(16, 9))
@@ -102,8 +105,9 @@ plotID = 0
 n = 0
 plotevery = 10
 dudtnorm = 1
+T = 60
 if not os.path.exists("initial_state.dat") or True:
-    while dudtnorm > 1e-5:
+    while problem.time < T:
         # plot
         if n % plotevery == 0:
             problem.plot(ax)
@@ -130,46 +134,38 @@ else:
     # load the initial state
     problem.load("initial_state.dat")
 
-# # start parameter continuation
-# problem.continuation_stepper.ds = 1e-2
-# problem.continuation_stepper.ndesired_newton_steps = 3
-# problem.settings.always_check_eigenvalues = True
-# problem.settings.always_locate_bifurcations = False
-# problem.settings.neigs = 20
 
-# constraint = TranslationConstraint(problem.ne)
-# problem.add_equation(constraint)
-
-# n = 0
-# plotevery = 5
-# while problem.ne.r > -0.016:
-#     # perform continuation step
-#     problem.continuation_step()
-#     n += 1
-#     print("step #:", n, " ds:", problem.continuation_stepper.ds)
-#     # plot
-#     if n % plotevery == 0:
-#         problem.plot(ax)
-#         fig.savefig("out/img/{:05d}.svg".format(plotID))
-#         plotID += 1
+problem.plot(ax)
+fig.savefig("out/img/{:05d}.svg".format(plotID))
+plotID += 1
 
 
-# # load the initial state and add extra dof for translation constraint
-# problem.remove_equation(constraint)
-# problem.load("initial_state.dat")
-# problem.add_equation(constraint)
+# start parameter continuation
+problem.continuation_stepper.ds = 1e-3
+problem.continuation_stepper.ds_max = 1e-2
+problem.continuation_stepper.ndesired_newton_steps = 3
+problem.settings.always_check_eigenvalues = False
+problem.settings.always_locate_bifurcations = False
+problem.settings.neigs = 0
 
-# # continuation in reverse direction
-# problem.new_branch()
-# problem.ne.r = -0.013
-# problem.continuation_stepper.ds = -1e-2
-# while problem.ne.r < -0.002:
-#     # perform continuation step
-#     problem.continuation_step()
-#     n += 1
-#     print("step #:", n, " ds:", problem.continuation_stepper.ds)
-#     # plot
-#     if n % plotevery == 0:
-#         problem.plot(ax)
-#         fig.savefig("out/img/{:05d}.svg".format(plotID))
-#         plotID += 1
+volume_constraint = VolumeConstraint(problem.ne)
+problem.add_equation(volume_constraint)
+translation_constraint = TranslationConstraint(problem.ne)
+problem.add_equation(translation_constraint)
+
+
+plt.close(fig)
+fig, ax = plt.subplots(2, 2, figsize=(16, 9))
+
+n = 0
+plotevery = 5
+while problem.ne.r > 0:
+    # perform continuation step
+    problem.continuation_step()
+    n += 1
+    print("step #:", n, " ds:", problem.continuation_stepper.ds)
+    # plot
+    if n % plotevery == 0:
+        problem.plot(ax)
+        fig.savefig("out/img/{:05d}.svg".format(plotID))
+        plotID += 1
