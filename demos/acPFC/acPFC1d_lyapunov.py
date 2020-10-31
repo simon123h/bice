45b91309a366692c1b96c8d02e65bc7005301473
simon123h <simon123h@users.noreply.github.com>
2020-10-31 11:37:13 +0100

    Add Profiler, update last10 mechanism and fix plot for single exponent

--- demos/acPFC/acPFC1d_lyapunov.py
+++ demos/acPFC/acPFC1d_lyapunov.py
@@ -8,6 +8,7 @@ sys.path.append("../..")  # noqa, needed for relative import of package
 from acPFC1d import acPFCProblem
 from bice import time_steppers
 from bice.measure import LyapunovExponentCalculator
+from bice import profile, Profiler
 
 
 # create output folder
@@ -42,8 +43,6 @@ if not os.path.exists("initial_state.dat"):
         n += 1
         # perform timestep
         problem.time_step()
-        # perform dealiasing
-        #problem.dealias()
         # calculate the new norm
         dudtnorm = np.linalg.norm(problem.rhs(problem.u))
         # catch divergent solutions
@@ -61,23 +60,37 @@ problem.time_stepper = time_steppers.BDF2(dt=0.1)
 lyapunov = LyapunovExponentCalculator(
     problem, nexponents=10, epsilon=1e-6, nintegration_steps=1)
 
-last10 = np.zeros((10, 10))
+last10 = []
 largest = []
 
+# Profiler.start()
+
+n = 1
+plotevery = 10
 while True:
+    # perform Lyapunov exponent calculation step
     lyapunov.step()
-    #problem.dealias()
-    last10[:-1] = last10[1:]
-    last10[-1] = lyapunov.exponents
+    # store last10 and largest Lyapunov exponents
+    last10 = [lyapunov.exponents] + last10[:9]
     largest += [np.max(lyapunov.exponents)]
-    ax_largest.clear()
-    ax_exponents.clear()
-    ccount = 0.
-    for exponents in last10:
-        ax_exponents.plot(exponents, marker='.', color='{:.1f}'.format(1 - ccount/10.), ls='')
-        ccount += 1
-    ax_exponents.plot(lyapunov.exponents, marker="o")
-    ax_largest.plot(largest)
-    fig.savefig("out/img/{:05d}.svg".format(plotID))
-    plotID += 1
     print("Lyapunov exponents:", lyapunov.exponents)
+    # plot
+    if n % plotevery == 0:
+        # clear axes
+        ax_largest.clear()
+        ax_exponents.clear()
+        if lyapunov.nexponents > 1:
+            # plot spectrum
+            for ccount, exponents in enumerate(last10):
+                ax_exponents.plot(exponents, marker='.', color='{:.1f}'.format(
+                    ccount/len(last10)), ls='')
+            ax_exponents.plot(lyapunov.exponents, marker="o")
+        else:
+            # plot solution
+            problem.plot(ax_exponents)
+        # plot largest exponent
+        ax_largest.plot(largest)
+        fig.savefig("out/img/{:05d}.svg".format(plotID))
+        plotID += 1
+        # show performance analysis
+        # Profiler.print_summary()
