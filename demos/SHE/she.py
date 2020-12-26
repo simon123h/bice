3fa7c691b28a07422657ed8fc8cfaf4aaa05923e
simon123h <simon123h@users.noreply.github.com>
2020-12-26 16:28:07 +0100

    Rename she_fd.py --> she.py and pseudospectral to she_ps.py

--- demos/SHE/she.py
+++ demos/SHE/she.py
@@ -4,48 +4,16 @@ import os
 import sys
 import numpy as np
 from scipy.sparse import diags
+import scipy.sparse as sp
 import matplotlib.pyplot as plt
 sys.path.append("../..")  # noqa, needed for relative import of package
 from bice import Problem, time_steppers
-from bice.pde import FiniteDifferencesEquation, PseudospectralEquation
-from bice.continuation import TranslationConstraint
+from bice.pde import FiniteDifferencesEquation
+from bice.continuation import ConstraintEquation
 from bice import profile, Profiler
 
 
-class SwiftHohenbergEquation(PseudospectralEquation):
-    r"""
-    Pseudospectral implementation of the 1-dimensional Swift-Hohenberg Equation
-    equation, a nonlinear PDE
-    \partial t h &= (r - (kc^2 + \Delta)^2)h + v * h^2 - g * h^3
-    """
-
-    def __init__(self, N, L):
-        super().__init__(shape=N)
-        # parameters
-        self.r = -0.013
-        self.kc = 0.5
-        self.v = 0.41
-        self.g = 1
-        # space and fourier space
-        self.x = [np.linspace(-L/2, L/2, N)]
-        self.build_kvectors(real_fft=True)
-        # initial condition
-        self.u = np.cos(
-            2 * np.pi * self.x[0] / 10) * np.exp(-0.005 * self.x[0]**2)
-
-    # definition of the SHE (right-hand side)
-    @profile
-    def rhs(self, u):
-        u_k = np.fft.rfft(u)
-        return np.fft.irfft((self.r - (self.kc**2 - self.k[0]**2)**2) * u_k) + self.v * u**2 - self.g * u**3
-
-    # definition of spatial derivative for translation constraint
-    def du_dx(self, u, direction=0):
-        du_dx = 1j*self.k[direction]*np.fft.rfft(u)
-        return np.fft.irfft(du_dx)
-
-
-class SwiftHohenbergEquationFD(FiniteDifferencesEquation):
+class SwiftHohenbergEquation(FiniteDifferencesEquation):
     r"""
     Finite difference implementation of the 1-dimensional Swift-Hohenberg Equation
     equation, a nonlinear PDE
@@ -59,14 +27,13 @@ class SwiftHohenbergEquationFD(FiniteDifferencesEquation):
         self.kc = 0.5
         self.v = 0.41
         self.g = 1
-        # space and fourier space
+        # spatial coordinate
         self.x = [np.linspace(-L/2, L/2, N)]
-        self.k = np.fft.rfftfreq(N, L / (2. * N * np.pi))
         # initial condition
         self.u = np.cos(2 * np.pi * self.x[0] / 10) * \
             np.exp(-0.005 * self.x[0] ** 2)
         # build finite difference matrices
-        self.build_FD_matrices()
+        self.build_periodic_FD_matrices()
         laplace = self.laplace
         self.linear_op = -2 * self.kc**2 * laplace - laplace.dot(laplace)
 
@@ -75,6 +42,7 @@ class SwiftHohenbergEquationFD(FiniteDifferencesEquation):
         return self.linear_op.dot(u) + (self.r - self.kc**4) * u + self.v * u**2 - self.g * u**3
 
     # definition of the Jacobian
+    @profile
     def jacobian(self, u):
         return self.linear_op + diags(self.r - self.kc**4 + self.v * 2 * u - self.g * 3 * u**2)
 
@@ -84,24 +52,62 @@ class SwiftHohenbergProblem(Problem):
     def __init__(self, N, L):
         super().__init__()
         # Add the Swift-Hohenberg equation to the problem
-        # self.she = SwiftHohenbergEquation(N, L)
-        self.she = SwiftHohenbergEquationFD(N, L)
+        self.she = SwiftHohenbergEquation(N, L)
         self.add_equation(self.she)
         # initialize time stepper
-        # self.time_stepper = time_steppers.RungeKuttaFehlberg45(dt=1e-3)
-        # self.time_stepper.error_tolerance = 1e-7
-        self.time_stepper = time_steppers.BDF(self)  # better for FD
+        self.time_stepper = time_steppers.BDF(self)
         # assign the continuation parameter
         self.continuation_parameter = (self.she, "r")
 
-    # set higher modes to null, for numerical stability
+
+class TranslationConstraint(ConstraintEquation):
+
+    def __init__(self, reference_equation):
+        # call parent constructor
+        super().__init__(shape=(1,))
+        # on which equation/unknowns should the constraint be imposed?
+        self.ref_eq = reference_equation
+        # initialize unknowns (velocity vector) to zero
+        self.u = np.zeros(1)
+
+    def rhs(self, u):
+        # set up the vector of the residual contributions
+        res = np.zeros((u.size))
+        # reference to the equation, shape and indices of the unknowns that we work on
+        eq = self.ref_eq
+        eq_idx = self.group.idx[eq]
+        self_idx = self.group.idx[self]
+        # obtain the unknowns
+        eq_u = u[eq_idx]
+        eq_u_old = self.group.u[eq_idx]
+        velocity = u[self_idx]
+        # add constraint to residuals of reference equation (velocity is the lagrange multiplier)
+        res[eq_idx] = velocity * eq.nabla.dot(eq_u)
+        # add the constraint equation
+        res[self_idx] = np.dot(eq.x[0], eq_u-eq_u_old)
+        # res[self_idx] = np.dot(eq_dudx, (eq_u - eq_u_old))
+        return res
+
     @profile
-    def dealias(self, fraction=1./2.):
-        u_k = np.fft.rfft(self.she.u)
-        N = len(u_k)
-        k = int(N*fraction)
-        u_k[k+1:-k] = 0
-        self.she.u = np.fft.irfft(u_k)
+    def jacobian(self, u):
+        # reference to the equation, shape and indices of the unknowns that we work on
+        eq = self.ref_eq
+        eq_idx = self.group.idx[eq]
+        self_idx = self.group.idx[self]
+        # obtain the unknowns
+        eq_u = u[eq_idx]
+        # eq_u_old = self.group.u[eq_idx]
+        velocity = u[self_idx][0]
+        # contribution of d(eq) / du
+        deq_du = velocity * eq.nabla
+        # contribution of d(eq) / dvelocity
+        deq_dv = eq.nabla.dot(eq_u).reshape((eq_u.size, 1))
+        # contribution of d(constraint) / du
+        dcnstr_du = eq.x[0].reshape((1, eq_u.size))
+        # contribution of d(constraint) / dvelocity
+        dcnstr_dv = None
+        # stack everything together and return
+        return sp.bmat([[deq_du, deq_dv], [dcnstr_du, dcnstr_dv]])
 
 
 # create output folder
@@ -166,10 +172,10 @@ while problem.she.r > -0.016:
     # plot
     if n % plotevery == 0:
         problem.plot(ax)
-        fig.savefig("out/img/{:05d}.svg".format(plotID))
+        fig.savefig("out/img/{:05d}.png".format(plotID))
         plotID += 1
 
-Profiler.print_summary(nested=True)
+Profiler.print_summary()
 
 # load the initial state and add extra dof for translation constraint
 problem.remove_equation(constraint)
@@ -188,5 +194,7 @@ while problem.she.r < -0.002:
     # plot
     if n % plotevery == 0:
         problem.plot(ax)
-        fig.savefig("out/img/{:05d}.svg".format(plotID))
+        fig.savefig("out/img/{:05d}.png".format(plotID))
         plotID += 1
+
+Profiler.print_summary()
