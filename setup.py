1bfc64c57193d862e638ac88b7fdd21509d7a0e6
simon123h <simon123h@users.noreply.github.com>
2021-02-05 09:56:58 +0100

    Add setup.py for local installation of package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bice',
+      version='0.1',
+      description='Numerical continuation and bifurcation package',
+      author='Simon Hartmann',
+      author_email='s.hartmann@wwu.de',
+      license='MIT',
+      packages=['bice'])
