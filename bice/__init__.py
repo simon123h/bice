6d24177679b391d1f94dc43ee8de86cf1925b5ca
simon123h <simon123h@users.noreply.github.com>
2021-12-02 13:18:42 +0100

    📝 Fix TODO in doc

--- bice/__init__.py
+++ bice/__init__.py
@@ -1,6 +1,7 @@
 """
-TODO: we shoud have a short description of bice here
+This is bice.
 """
+# TODO: we need a short description of bice here
 
 # import core namespace, so we can e.g. use bice.Problem
 from .core import *
