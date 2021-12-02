96a77048392cd75206bbadbae73b314890b1209f
simon123h <simon123h@users.noreply.github.com>
2021-12-02 13:54:21 +0100

    :bug: Remove old reference to DeflatedContinuation

--- bice/continuation/__init__.py
+++ bice/continuation/__init__.py
@@ -2,14 +2,13 @@
 
 from .continuation_steppers import NaturalContinuation
 from .continuation_steppers import PseudoArclengthContinuation
-from .continuation_steppers import DeflatedContinuation
 from .constraints import ConstraintEquation, VolumeConstraint, TranslationConstraint
 from .bifurcations import BifurcationConstraint
 from .timeperiodic import TimePeriodicOrbitHandler
 
 
 __all__ = [
-    'NaturalContinuation', 'PseudoArclengthContinuation', 'DeflatedContinuation',
+    'NaturalContinuation', 'PseudoArclengthContinuation',
     'ConstraintEquation', 'VolumeConstraint', 'TranslationConstraint',
     'BifurcationConstraint',
     'TimePeriodicOrbitHandler'
