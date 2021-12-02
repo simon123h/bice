01cc239663444a5439febdea8174ea1475141d5a
simon123h <simon123h@users.noreply.github.com>
2021-12-02 15:25:48 +0100

    Update version number

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Simon Hartmann'
 author = 'Simon Hartmann'
 
 # The full version, including alpha/beta/rc tags
-release = '2021'
+release = '0.2'
 
 
 # -- General configuration ---------------------------------------------------
