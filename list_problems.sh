9f56a470141feb2979e7dbf3c88b2399dafc8eb6
simon123h <simon123h@users.noreply.github.com>
2021-12-02 12:59:25 +0100

    :fire: Remove list_problems.sh

--- list_problems.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DISABLED=invalid-name,fixme,too-few-public-methods,too-many-instance-attributes,missing-function-docstring
-
-pylint --disable=$DISABLED --output-format=colorized bice
